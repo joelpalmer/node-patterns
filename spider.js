65866003cd283e9db1730361adb6cc1c2aad91a4
Joel Palmer <joelpalmer@gmail.com>
2019-01-03 16:24:47 -0600

    factored out download and savefile functionality in to their own funcs

--- spider.js
+++ spider.js
@@ -7,27 +7,41 @@ const utilities = require("./utilities");
 function spider(url, callback) {
   const filename = utilities.urlToFilename(url);
   fs.exists(filename, exists => {
-    if (!exists) {
-      console.log(`Downloading ${url}`);
-      request(url, (err, response, body) => {
-        if (err) {
-          return callback(err);
-        }
-        mkdirp(path.dirname(filename), err => {
-          if (err) {
-            return callback(err);
-          }
-          fs.writeFile(filename, body, err => {
-            if (err) {
-              return callback(err);
-            }
-            callback(null, filename, true);
-          });
-        });
-      });
-    } else {
-      callback(null, filename, false);
+    if (exists) {
+      return callback(null, filename, false);
     }
+    download(url, filename, err => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, filename, true);
+    });
+  });
+}
+
+function saveFile(filename, contents, callback) {
+  mkdirp(path.dirname(filename), err => {
+    if (err) {
+      return callback(err);
+    }
+    fs.writeFile(filename, contents, callback);
+  });
+}
+
+function download(url, filename, callback) {
+  console.log(`Downloading ${url}`);
+  request(url, (err, response, body) => {
+    if (err) {
+      return callback(err);
+    }
+
+    saveFile(filename, body, err => {
+      if (err) {
+        return callback(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      callback(null, body);
+    });
   });
 }
 
