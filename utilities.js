af49016ad93f75919f247cb17b068c986924c8b2
Joel Palmer <joelpalmer@gmail.com>
2019-01-10 10:40:57 -0600

    add promisify utility function

--- utilities.js
+++ utilities.js
@@ -47,3 +47,22 @@ module.exports.getPageLinks = function getPageLinks(currentUrl, body) {
       return !!element;
     });
 };
+
+module.exports.promisify = function(callbackBasedApi) {
+  return function promisified() {
+    const args = [].slice.call(arguments);
+    return new Promise((resolve, reject) => {
+      args.push((err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        if (arguments.length <= 2) {
+          resolve(result);
+        } else {
+          resolve([].slice.call(arguments, 1));
+        }
+      });
+      callbackBasedApi.apply(null, args);
+    });
+  }
+};
