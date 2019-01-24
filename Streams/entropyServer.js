70bcd36caa83838bcd30c7308ee32e0322595878
Joel Palmer <joelpalmer@gmail.com>
2019-01-24 16:21:47 -0600

    handle highWaterMark limit in entropyServer

--- Streams/entropyServer.js
+++ Streams/entropyServer.js
@@ -1,12 +1,22 @@
-const Chance = require('chance');
+const Chance = require("chance");
 const chance = new Chance();
 
-require('http').createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    while(chance.bool({likelihood: 95})) {
-        res.write(chance.string() + '\n');
+require("http")
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    function generateMore() {
+      while (chance.bool({ likelihood: 95 })) {
+        let shouldContinue = res.write(
+          chance.string({ length: 16 * 1024 - 1 })
+        );
+        if (!shouldContinue) {
+          console.log("Backpressure");
+          return res.once("drain", generateMore);
+        }
+      }
+      res.end("\nThe end...\n", () => console.log("All data sent"));
     }
-    res.end('\nThe end...\n');
-    res.on('finish', () => console.log('All data sent'));
 
-}).listen(8080, () => console.log('Listening on http://localhost:8080'));
+    generateMore();
+  })
+  .listen(8080, () => console.log("Listening on http://localhost:8080"));
