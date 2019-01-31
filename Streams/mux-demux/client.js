1c3e09c508f62b7a135fe4f042bbb5b808864022
Joel Palmer <joelpalmer@gmail.com>
2019-01-31 10:13:06 -0600

    add multiplexChannels func to mux client

--- Streams/mux-demux/client.js
+++ Streams/mux-demux/client.js
@@ -1,2 +1,25 @@
-const child_process = require('child_process');
-const net = require('net');
+const child_process = require("child_process");
+const net = require("net");
+
+function multiplexChannels(sources, destination) {
+	let totalChannels = sources.length;
+	for (let i = 0; i < sources.length; i++) {
+		sources[i]
+			.on("readable", function() {
+				let chunk;
+				while ((chunk = this.read()) !== null) {
+					const outBuff = new Buffer(1 + 4 + chunk.length);
+					outBuff.writeUInt8(i, 0);
+					outBuff.writeUInt32BE(chunk.length, 1);
+					chunk.copy(outBuff, 5);
+					console.log("Sending packet to channel: " + i);
+					destination.write(outBuff);
+				}
+			})
+			.on("end", () => {
+				if (--totalChannels === 0) {
+					destination.end();
+				}
+			});
+	}
+}
