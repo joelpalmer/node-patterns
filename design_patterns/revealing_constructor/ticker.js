1c1abea89452cbe84f7c797aa72f06e9ddb1a90a
Joel Palmer <joelpalmer@gmail.com>
2019-01-31 14:07:12 -0600

    roee and ticker

---
+++ design_patterns/revealing_constructor/ticker.js
@@ -0,0 +1,7 @@
+const Roee = require('./roee');
+
+const ticker = new Roee((emit) => {
+    let tickCount = 0;
+    setInterval(() => emit('tick', tickCount++), 1000);
+});
+module.exports = ticker;
