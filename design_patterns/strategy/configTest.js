ffa51e1d86d7d6f306e6eca72ef94c34a722be24
Joel Palmer <joelpalmer@gmail.com>
2019-02-01 15:23:08 -0600

    add configTest mod

---
+++ design_patterns/strategy/configTest.js
@@ -0,0 +1,13 @@
+const Config = require('./config');
+const strategies = require('./strategies');
+
+const jsonConfig = new Config(strategies.json);
+jsonConfig.read('samples/conf.json');
+jsonConfig.set('family.name', 'Palmer');
+jsonConfig.save('samples/conf_mod.json');
+
+
+const iniConfig = new Config(strategies.ini);
+iniConfig.read('samples/conf.ini');
+iniConfig.set('family.name', 'Palmer');
+iniConfig.save('samples/conf_mod.ini');
