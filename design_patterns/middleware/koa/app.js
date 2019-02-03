14be6152419f614c3e031ef684ca2f38dd1fecea
Joel Palmer <joelpalmer@gmail.com>
2019-02-03 13:08:47 -0600

    finish koa middleware

--- design_patterns/middleware/koa/app.js
+++ design_patterns/middleware/koa/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
-app.use(function * () { // generator support going away in v3. Need to investigate
+app.use(require('./rateLimit'));
+app.use(function * () { // generator support going away in v3 in favor of async/await
     this.body = {"now": new Date()};
 });
 
---
+++ design_patterns/middleware/koa/rateLimit.js
@@ -0,0 +1,15 @@
+const lastCall = new Map();
+
+module.exports = function*(next) {
+	const now = new Date();
+	if (
+		lastCall.has(this.ip) &&
+		now.getTime() - lastCall.get(this.ip).getTime() < 1000
+	) {
+		return (this.status = 429);
+    }
+    yield next;
+
+    lastCall.set(this.ip, now);
+    this.set('X-RateLimit-Reset', now.getTime() + 1000);
+};
